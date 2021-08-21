ee9fd90cb69210829c02bfb0e17b4b98c6596028
wnstjd4778 <smy4778@naver.com>
2021-08-21 20:17:39 +0900

    2021-08-21 소셜로그인 게시판 구현

--- src/main/java/com/example/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
+++ src/main/java/com/example/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
@@ -2,6 +2,9 @@ package com.example.freelecspringboot2webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class FreelecSpringboot2WebserviceApplication {
